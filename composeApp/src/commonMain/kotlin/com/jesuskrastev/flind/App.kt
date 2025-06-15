576b756eb61d64387ff3eebd946bd34d950dd724
JesusKrastev <jesuskrastev@gmail.com>
2025-06-15 12:24:04 +0200

    feat: Created TopAppBar component

--- composeApp/src/commonMain/kotlin/com/jesuskrastev/flind/App.kt
+++ composeApp/src/commonMain/kotlin/com/jesuskrastev/flind/App.kt
@@ -1,16 +1,13 @@
 package com.jesuskrastev.flind
 
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import com.jesuskrastev.flind.core.theme.FlindTheme
 import com.jesuskrastev.flind.features.components.BottomNavigationBar
+import com.jesuskrastev.flind.features.explore.presentation.ExploreScreen
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
@@ -26,7 +23,7 @@ fun App() {
                     BottomNavigationBar()
                 }
             ) {
-
+                ExploreScreen()
             }
         }
     }
---
+++ composeApp/src/commonMain/kotlin/com/jesuskrastev/flind/features/components/TopAppBar.kt
@@ -0,0 +1,32 @@
+package com.jesuskrastev.flind.features.components
+
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TopAppBar(
+    modifier: Modifier = Modifier,
+    title: String,
+    navigationIcon: @Composable () -> Unit = {},
+    actions: @Composable RowScope.() -> Unit = {},
+) {
+    CenterAlignedTopAppBar(
+        modifier = modifier,
+        title = {
+            Text(
+                text = title,
+                style = MaterialTheme.typography.bodyLarge,
+                fontWeight = FontWeight.Bold
+            )
+        },
+        actions = actions,
+        navigationIcon = navigationIcon,
+    )
+}
